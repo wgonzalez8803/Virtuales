952bae15738459005730d420319c8e95225e334a
William González <wgonzalez8803@gmail.com>
2021-05-21 06:16:54 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html >
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial.scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie-edge" >
+	
+	<title>Virtual-Es</title>
+	<link rel="shortcut icon" href="img/favicon.png">
+	<script type="text/javascript" src="popup.js"></script>
+	<script type="text/javascript" src="js/jquery-3.3.1.slim.min.js"></script>
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+
+</head>
+<body>
+ <!--Header Home-->      
+ <header class="headervirtuales">
+    <div class="container">
+      <div class="row ">
+        <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a href="">
+                    <img src="img/logo-virtual-es.svg" style="width: 200px;"/>
+                  </a>              
+            </nav>
+        </div>
+    </div>
+   </div>
+  </header>  
+<div class="container">
+  
+
+  <section>
+  <div class="row">
+      <div class="col-sm-6 col-md-6 col-lg-6 col-xl-6">
+      	 <div class="row">
+      	 <div class="tituloponencia formtext">TÍTULO DE LA PONENCIA</div>
+      	 </div>
+      	 <div class="row">
+      	 <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 horario  formtext">
+         <img id="imguser" src="img/human-greeting.svg" style="width: 6%;"/>
+          Juan Peréz / Desarrollador
+         </div>
+      	 </div>
+      	 <div class="row">
+      	 <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12  horario formtext">
+      	 <img id="imguser" src="img/reloj-contorno-circular.svg" style="width: 5%;"/>
+      	   08:00 a 08:30 / Col - Perú
+      	 </div>
+      	 </div>
+  	  </div>
+
+  	  <div class="col-sm-3 col-md-3 col-lg-3 col-xl-3">
+   	  </div>
+  	  <div class="col-sm-3 col-md-3 col-lg-3 col-xl-3 alignr">
+  	  	<nav class="navbar navbar-expand-lg navbar-light">
+                <a href="">
+                    <img src="img/logo-virtual-es.svg" style="width: 220px;"/>
+                  </a>              
+            </nav>
+  	  </div>
+  </div>
+  <div class="row">
+  	  <div class="col-sm-8 col-md-8 col-lg-8 col-xl-8">
+  	  	<div class="row">
+  	  	 <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 ">
+  	  	<iframe width="100%" height="310" src="https://www.youtube.com/embed/k2wJ32nFY-Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  	  	</div>
+  	  </div>
+  	  <div class="row">
+  	  	 <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12  txtinf">
+  	  	 Texto informativo - Noticias y llamadas
+  	  	</div>
+  	  </div>
+  	  <div class="row">
+  	  	 <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12  ">
+  	  	 
+<p>
+  <button class="btn btn-outline-light" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+   Informacion del ponente
+  </button>
+ 
+  <button class="btn btn-outline-light" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+   Recursos
+  </button>
+
+</p>
+<div class="collapse" id="collapseExample">
+  <div class="card card-body infponente">
+    Información ponente Test
+  </div>
+</div>
+
+
+
+  	  	</div>
+  	  </div>
+
+  	 </div>	
+  	  <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 ">
+  	  <div class="row">
+  	  	 <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 nevento">
+  	  	 	Nombre del Evento
+  	  	</div>
+  	  </div>
+  	  <div class="row">
+  	  	 <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 byevento">
+  	  	 	Evento Por: @MICROSOFT
+  	  	</div>
+  	  </div>
+  	  <div class="row">
+  	  	 <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 formtext">
+  	  	 	Mayo 20 2021
+  	  	</div>
+  	  </div>
+  	  <div class="row">
+  	  	 <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 ">
+  	  	     <div class="labelcompartir">
+  	  	     	<img id="imguser" src="img/share.svg" style="width: 20%;"/>
+  	  	     	compartir<div>
+   	  	</div>
+  	  </div>
+  	  <div class="row">
+  	  	 <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+  	  	 	<iframe width="100%" height="220" src="https://www.youtube.com/embed/k2wJ32nFY-Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  	  	</div>
+  	  </div>
+  </div>
+
+         <!--Header Home-->
+
+</div>
+</div>
+<div class="fixed-bottom  float-left">
+
+	<div class="container">
+      <div class="row ">
+        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <p class="formtext">Copyright 2021 © Virtual-Es   <a href="https://google.com"> Política de privacidad</a></p>
+        </div>
+       
+    </div>
+   </div>
+	
+</div>
+</body>
+</html>
